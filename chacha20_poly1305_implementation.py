4bf1bb057e5f333431a8e809ca95e4b65e602e88
mai26034-stud <mai26034@uom.edu.gr>
2026-01-05 21:50:37 +0200

    Create chacha20_poly1305_implementation.py

---
+++ chacha20_poly1305_implementation.py
@@ -0,0 +1,20 @@
+import nacl.secret
+import nacl.utils
+from aead.base import AeadCipher
+
+class ChaChaPolyCipher(AeadCipher):
+    KEY_SIZE = nacl.secret.SecretBox.KEY_SIZE
+    NONCE_SIZE = nacl.secret.SecretBox.NONCE_SIZE  # 192-bit
+
+    def __init__(self, key: bytes):
+        self.box = nacl.secret.SecretBox(key)
+
+    @staticmethod
+    def generate_key() -> bytes:
+        return nacl.utils.random(ChaChaPolyCipher.KEY_SIZE)
+
+    def encrypt(self, nonce: bytes, aad: bytes, plaintext: bytes) -> bytes:
+        return self.box.encrypt(plaintext, nonce, aad)
+
+    def decrypt(self, nonce: bytes, aad: bytes, ciphertext: bytes) -> bytes:
+        return self.box.decrypt(ciphertext, nonce, aad)
