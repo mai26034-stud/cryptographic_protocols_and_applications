e2f34b920c3da90bf9538612fbc4e23b23477b65
mai26034-stud <mai26034@uom.edu.gr>
2026-01-16 17:03:41 +0200

    Update chacha20_poly1305_cipher.py

--- chacha20_poly1305_cipher.py
+++ chacha20_poly1305_cipher.py
@@ -1,20 +1,26 @@
-import nacl.secret
-import nacl.utils
+import os
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from aead_interface import AeadCipher
 
 class ChaChaPolyCipher(AeadCipher):
-    KEY_SIZE = nacl.secret.SecretBox.KEY_SIZE
-    NONCE_SIZE = nacl.secret.SecretBox.NONCE_SIZE
+    KEY_SIZE = 32
+    NONCE_SIZE = 12
 
     def __init__(self, key: bytes):
-        self.box = nacl.secret.SecretBox(key)
+        if len(key) != self.KEY_SIZE:
+            raise ValueError(f"Key must be {self.KEY_SIZE} bytes long")
+        self.chacha = ChaCha20Poly1305(key)
 
     @staticmethod
     def generate_key() -> bytes:
-        return nacl.utils.random(ChaChaPolyCipher.KEY_SIZE)
+        return os.urandom(ChaChaPolyCipher.KEY_SIZE)
 
     def encrypt(self, nonce: bytes, aad: bytes, plaintext: bytes) -> bytes:
-        return self.box.encrypt(plaintext, nonce, aad)
+        if len(nonce) != self.NONCE_SIZE:
+            raise ValueError(f"Nonce must be {self.NONCE_SIZE} bytes long")
+        return self.chacha.encrypt(nonce, plaintext, aad)
 
     def decrypt(self, nonce: bytes, aad: bytes, ciphertext: bytes) -> bytes:
-        return self.box.decrypt(ciphertext, nonce, aad)
+        if len(nonce) != self.NONCE_SIZE:
+            raise ValueError(f"Nonce must be {self.NONCE_SIZE} bytes long")
+        return self.chacha.decrypt(nonce, ciphertext, aad)
